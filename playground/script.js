8da3162d65f794b6acb1a4a18655701c46451e06
Mario Lazzari <mario.lazzari@gmail.com>
2021-04-15 20:15:23 +0200

    Done lesson 9

--- playground/script.js
+++ playground/script.js
@@ -2,10 +2,21 @@
 let a = 1;
 let b = 2;
 let c = 3;
-
-a = 2;
-
 let d = a;
 
 console.log(a);
 console.log(d);
+
+// Number
+let int = 1;
+let float = 1.1;
+
+console.log(typeof int);
+console.log(typeof float);
+console.log(float + int);
+console.log("(a+b)/c = ", (a + b) / c);
+
+a = 1.2;
+b = 2.2;
+console.log("a+b=", a + b);
+console.log("a*b=", a * b);
